4445f8eaafa7baa35798d844d677ef1f000ed7b6
Joao-Pedro-de-Souza-dos-Santos <joaopedrodesouzadossantos15@gmail.com>
2024-01-03 21:16:43 -0300

    add base styled @types pages style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {App} from './pages/App/index.tsx'
+import { GlobalStyles } from './styles/reset.ts'
+import { ThemeProvider } from 'styled-components'
+import { appTheme } from './styles/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={appTheme}>
     <App />
+    <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/App/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+export function App() {
+  return (
+    <Container>
+      <header>Header</header>
+      <main><h1>Pokedex</h1></main>
+      <footer>Footer</footer>
+    </Container>
+  );
+}
