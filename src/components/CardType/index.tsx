ded7d42a51afef688ff6ead73613a94ae6902804
Joao-Pedro-de-Souza-dos-Santos <joaopedrodesouzadossantos15@gmail.com>
2024-01-09 19:17:10 -0300

    add PokemonCard and CardType!

--- src/components/CardType/index.tsx
+++ src/components/CardType/index.tsx
@@ -1,8 +1,8 @@
 import { PokemonType } from "../../@types/pokemon";
 import { Container } from "./style";
 
-export type TypePorps = { type: PokemonType };
+export type TypePorps = { type: PokemonType; size?: number}; //Tipagem Explícita, pode ou não ser definida "size?"
 
-export function CardType({ type }: TypePorps) {
-  return <Container type={type}>{type}</Container>;
+export function CardType({ type,  size = 10 }: TypePorps) {
+  return <Container type={type} size={size}>{type}</Container>;
 }
