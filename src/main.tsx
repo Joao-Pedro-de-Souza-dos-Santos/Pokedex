609a481a792176729484071425174ef2409aa684
João Pedro de Souza <143228303+Joao-Pedro-de-Souza-dos-Santos@users.noreply.github.com>
2024-02-11 22:24:12 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './pages/App/index.tsx'
 import { GlobalStyles } from './styles/reset.ts'
 import { ThemeProvider } from 'styled-components'
 import { appTheme } from './styles/theme.ts'
